4e0ea7442211f057b63572a8ec6f9b27cb1cbe1a
Juan David García Manrique <jdgarcia1381@gmail.com>
2025-09-22 18:36:28 -0500

    Add FileField into Libro model

--- clase21/drf/drf/urls.py
+++ clase21/drf/drf/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -31,3 +33,5 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include("libros.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- clase21/drf/libros/models.py
+++ clase21/drf/libros/models.py
@@ -6,6 +6,7 @@ class Libro(models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
     anio_publicacion = models.IntegerField()
+    imagen_portada = models.FileField()
 
     def __str__(self):
         return f"{self.titulo} - {self.autor}"
